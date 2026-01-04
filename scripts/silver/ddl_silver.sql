7431cc11ec83f010a41ceac15d632fe3f768a0e7
Adinath Dnyanadev Kadam <130644436+kadamadinath@users.noreply.github.com>
2026-01-04 22:05:58 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,1 +1,82 @@
+--------------------------------------------
+--DDL Script for  Silver Layer(All Tables)--
+--------------------------------------------
+IF OBJECT_ID('silver.crm_cust_info','U') IS NOT NULL
+ DROP TABLE silver.crm_cust_info;
+ CREATE TABLE silver.crm_cust_info 
+ (
+	 cst_id INT,
+	 cst_key NVARCHAR(55),
+	 cst_firstname NVARCHAR(55),
+	 cst_lastname NVARCHAR(55),
+	 cst_marital_status NVARCHAR(55),
+	 cst_gndr NVARCHAR(55),
+	 cst_create_date DATE,
+	 dwh_create_date DATETIME2 DEFAULT GETDATE()
+ );
+ 
 
+ IF OBJECT_ID('silver.crm_prd_info','U') IS NOT NULL
+ DROP TABLE silver.crm_prd_info;
+ CREATE TABLE silver.crm_prd_info
+ (
+	prd_id INT,
+	prd_cat_id NVARCHAR(55),
+	prd_key NVARCHAR(55),
+	prd_nm NVARCHAR(55),
+	prd_cost INT,
+	prd_line NVARCHAR(55),
+	prd_start_dt DATE,
+	prd_end_dt DATE,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+ );
+
+
+ IF OBJECT_ID('silver.crm_sales_details','U') IS NOT NULL
+ DROP TABLE silver.crm_sales_details;
+ CREATE  TABLE silver.crm_sales_details
+ (
+	sls_ord_num NVARCHAR(55),
+	sls_prd_key NVARCHAR(55),
+	sls_cust_id INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT,	
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+ );
+ 
+
+ IF OBJECT_ID('silver.erp_cust_az12','U') IS NOT NULL
+ DROP TABLE silver.erp_cust_az12;
+ CREATE TABLE silver.erp_cust_az12 
+ (
+	CID NVARCHAR(55),
+	BDATE DATE,
+	GEN NVARCHAR(55),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+ );
+ 
+
+ IF OBJECT_ID('silver.erp_loc_a101','U') IS NOT NULL
+ DROP TABLE silver.erp_loc_a101;
+ CREATE TABLE silver.erp_loc_a101
+ (
+	CID NVARCHAR(55),
+	CNTRY NVARCHAR(55),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+ );
+ 
+
+ IF OBJECT_ID('silver.erp_px_cat_g1v2','U') IS NOT NULL
+ DROP TABLE silver.erp_px_cat_g1v2;
+ CREATE TABLE silver.erp_px_cat_g1v2
+ (
+	ID NVARCHAR(55),
+	CAT NVARCHAR(55),
+	SUBCAT NVARCHAR(55),
+	MAINTENANCE NVARCHAR(55),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+ );
